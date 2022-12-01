08b5223461a4133527a84708c667bfb3e7407191
Slava Okunev <slava.okunev@dualbootpartners.com>
2022-12-01 16:14:38 +0400

    [#3102] devise controllers & views & main page & redirects

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users, controllers: {
+    confirmations: "users/confirmations",
+    passwords: "users/passwords",
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+  }
+  root "main_pages#index"
+  get "users/:id", to: "users#show", as: 'user'
+  
 end
